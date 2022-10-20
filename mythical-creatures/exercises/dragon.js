40b7e8aa2503e1e09c1dbd9f0d22e41d4ddc19c0
Ciera DePauw <cieragrace18@gmail.com>
2022-10-20 15:21:14 -0600

    add completed Dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,28 @@
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.eatCount = 0;
+  }
 
+  greet(rider) {
+    return `Hi, ${this.rider}!`
+  }
+  eat() {
+  this.eatCount += 1
+    if (this.eatCount >= 3 ) {
+      this.hungry = false
+    }
+}
+}
+
+
+
+
+
+
+
+
+
+module.exports = Dragon;
